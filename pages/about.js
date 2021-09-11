dc28ce580cc58b84bb272cdddc0e471f8e041947
andykimchris <andrew.muchiri97@gmail.com>
2021-09-11 18:55:02 +0300

    added about & home pages

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            Test about
+            <p>Lorem ipsum dolor sit amet, consectet</p>    
+            <p>Lorem ipsum dolor sit amet, consectet</p>    
+        </div>
+     )
+}
+ 
+export default About
---
+++ pages/test-bootstrap.js
@@ -0,0 +1,25 @@
+import styles from "../styles/TestBootstrap.module.scss";
+
+const Bootstrap = () => {
+    return ( 
+      <div className={styles.container}>
+        <div className="row">
+          <div className="col">
+            First in DOM, no order applied
+          </div>
+          <div className="col order-5">
+            Second in DOM, with a larger order
+            <button type="button" className="btn btn-primary">Primary</button>
+            <button type="button" className="btn btn-success">Success</button>
+            <button type="button" className={styles.btn1}>BTN 1</button>
+
+          </div>
+          <div className="col order-1">
+            Third in DOM, with an order of 1
+          </div>
+        </div>
+    </div>
+     )
+}
+ 
+export default Bootstrap;

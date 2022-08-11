ab6541e896134cdd659fa95c6a20e66fd0ffe4cd
andykimchris <andrew.kimani@bejamas.io>
2022-08-11 10:49:39 +0300

    p

--- pages/test-bootstrap.js
+++ pages/test-bootstrap.js
@@ -1,25 +1,26 @@
-import styles from "../styles/TestBootstrap.module.scss";
+import styles from "../styles/TestBootstrap.css";
 
 const Bootstrap = () => {
-    return ( 
-      <div className={styles.container}>
-        <div className="row">
-          <div className="col">
-            First in DOM, no order applied
-          </div>
-          <div className="col order-5">
-            Second in DOM, with a larger order
-            <button type="button" className="btn btn-primary">Primary</button>
-            <button type="button" className="btn btn-success">Success</button>
-            <button type="button" className={styles.btn1}>BTN 1</button>
-
-          </div>
-          <div className="col order-1">
-            Third in DOM, with an order of 1
-          </div>
+  return (
+    <div className={styles.container}>
+      <div className="row">
+        <div className="col">First in DOM, no order applied</div>
+        <div className="col order-5">
+          Second in DOM, with a larger order
+          <button type="button" className="btn btn-primary">
+            Primary
+          </button>
+          <button type="button" className="btn btn-success">
+            Success
+          </button>
+          <button type="button" className={styles.btn1}>
+            BTN 1
+          </button>
         </div>
+        <div className="col order-1">Third in DOM, with an order of 1</div>
+      </div>
     </div>
-     )
-}
- 
+  );
+};
+
 export default Bootstrap;
